ff401ffcd64cb15e94dbe2572b08a0355ba33ee1
uday-sudo <udayavengerdude@gmail.com>
2025-05-26 20:44:49 +0530

    worflow trial

--- hosts/common/global/packages.nix
+++ hosts/common/global/packages.nix
@@ -6,7 +6,7 @@
 }: {
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  nix.settings.trusted-users = [ "root" "uday" "hooman" ];
+  nix.settings.trusted-users = [ "root" "uday" "hooman" "forgejo"];
   environment.systemPackages = with pkgs; [
     acpi
     brightnessctl
