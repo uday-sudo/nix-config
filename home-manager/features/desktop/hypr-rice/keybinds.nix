905094a70da6fa22c4e666a3e6aba6efc11bbcb3
uday-sudo <udayavengerdude@gmail.com>
2026-02-14 23:17:43 +0530

    [UPDATE] Changes to hyprland keybinds, still a WIP

--- home-manager/features/desktop/hypr-rice/keybinds.nix
+++ home-manager/features/desktop/hypr-rice/keybinds.nix
@@ -1,31 +1,35 @@
 {
+  lib,
+  pkgs,
+  ...
+}: {
   wayland.windowManager.hyprland.settings = {
     "$mod" = "SUPER";
-    bind = [
-      ", Print, exec, grimblast copy area"
-      "$mod, D, exec, rofi -show drun"
-      "$mod, RETURN, exec, ${lib.getExe pkgs.ghostty};"
-      "$mod, W, killactive"
-      "$mod, T, togglefloating"
-      "$mod, F, fullscreen"
-      "$mod Shift, F, fullscreenstate"
-      "$mod Shift, Q, exit"
-    ]
-    ++ (
-      # workspaces
-      # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-      builtins.concatLists (
-        builtins.genList (
-          i:
-          let
-            ws = i + 1;
-          in
-          [
-            "$mod, code:1${toString i}, workspace, ${toString ws}"
-            "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-          ]
-        ) 9
-      )
-    );
+    bind =
+      [
+        ", Print, exec, grimblast copy area"
+        "$mod, D, exec, rofi -show drun"
+        "$mod, RETURN, exec, ${lib.getExe pkgs.ghostty};"
+        "$mod, W, killactive"
+        "$mod, T, togglefloating"
+        "$mod, F, fullscreen"
+        "$mod Shift, F, fullscreenstate"
+        "$mod Shift, Q, exit"
+      ]
+      ++ (
+        # workspaces
+        # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+        builtins.concatLists (
+          builtins.genList (
+            i: let
+              ws = i + 1;
+            in [
+              "$mod, code:1${toString i}, workspace, ${toString ws}"
+              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+            ]
+          )
+          9
+        )
+      );
   };
 }
