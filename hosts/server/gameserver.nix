6470f52b078f731b694cc5449321c0e21919dfa3
uday-sudo <udayavengerdude@gmail.com>
2026-04-12 21:23:14 +0530

    [FIX] Fixed pufferpanel unable to run stuff

--- hosts/server/gameserver.nix
+++ hosts/server/gameserver.nix
@@ -9,7 +9,24 @@
   svcName = "game";
   webPort = 7566;
   sftpPort = 5657;
+  # PufferPanel's systemd StateDirectory under /var/lib/pufferpanel is mounted
+  # noexec, which breaks game server binaries like Terraria. Keep executable
+  # server data under /srv so PufferPanel can launch downloaded binaries.
+  dataDir = "/srv/pufferpanel";
 in {
+  users.users.pufferpanel = {
+    isSystemUser = true;
+    group = "pufferpanel";
+  };
+
+  users.groups.pufferpanel = {};
+
+  systemd.tmpfiles.rules = [
+    "d ${dataDir} 0750 pufferpanel pufferpanel - -"
+    "d ${dataDir}/servers 0750 pufferpanel pufferpanel - -"
+    "d ${dataDir}/binaries 0750 pufferpanel pufferpanel - -"
+  ];
+
   services.caddy = {
     virtualHosts."${svcName}.${domain}" = {
       extraConfig = ''
@@ -27,17 +44,48 @@ in {
       "podman"
     ];
     extraPackages = [
+      pkgs.bash
+      pkgs.curl
+      pkgs.gawk
+      pkgs.gnutar
+      pkgs.gzip
       pkgs.openjdk
       pkgs.unzip
-      pkgs.bash
     ];
+    package = pkgs.buildFHSEnv {
+      name = "pufferpanel-fhs";
+      runScript = lib.getExe pkgs.pufferpanel;
+      targetPkgs = pkgs':
+        with pkgs'; [
+          bash
+          coreutils
+          curl
+          gawk
+          gnutar
+          gzip
+          icu
+          openssl
+          unzip
+          zlib
+        ];
+    };
     environment = {
       PUFFER_WEB_HOST = ":${toString webPort}";
       PUFFER_DAEMON_SFTP_HOST = ":${toString sftpPort}";
       PUFFER_DAEMON_CONSOLE_BUFFER = "1000";
       PUFFER_DAEMON_CONSOLE_FORWARD = "true";
       PUFFER_PANEL_REGISTRATIONENABLED = "false";
+      # systemd mounts StateDirectory under /var/lib/pufferpanel with noexec.
+      # Keep executable server data outside that tree so game binaries can run.
+      PUFFER_DAEMON_DATA_SERVERS = "${dataDir}/servers";
+      PUFFER_DAEMON_DATA_BINARIES = "${dataDir}/binaries";
     };
+  };
+
+  systemd.services.pufferpanel.serviceConfig = {
+    DynamicUser = lib.mkForce false;
+    User = lib.mkForce "pufferpanel";
+    Group = lib.mkForce "pufferpanel";
   };
   # systemd.services.pufferpanel.preStart = let
   #   adminCmd = "${lib.getExe config.services.pufferpanel.package}";
